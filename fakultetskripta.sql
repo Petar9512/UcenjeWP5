1e8e407e9d84271a88a3f8810e33f757c74a7d01
Petar9512 <petarjelicic95@gmail.com>
2024-05-27 21:00:46 +0200

    Update fakultetskripta.sql

--- fakultetskripta.sql
+++ fakultetskripta.sql
@@ -1,10 +1,44 @@
 --create database fakultet;
 --use fakultet;
 
-create table IspitniRok(
-sifra int,
-predmet varchar(50),
-vrstaIspita varchar(50),
-datum datetime,
-pristupio bit
-);
+--create table Katedre(
+--katedraID int,
+--naziv varchar(100),
+--predstojnik varchar(50)
+--);
+
+--create table Smjerovi(
+--smjerID int,
+--naziv varchar(100),
+--brojStudenata int
+--);
+
+--create table Kolegiji(
+--kolegijID int,
+--katedraID int,
+--smjerID int,
+--naziv varchar(100),
+--predavaè varchar(50),
+--obavezni bit
+--);
+
+--create table Studenti(
+--studentID int,
+--smjerID int,
+--ime varchar(30),
+--prezime varchar(30),
+--oib char(11)
+--);
+
+--create table IspitniRok(
+--ispitniRokID int,
+--kolegijID int,
+--vrstaIspita varchar(50),
+--datum datetime
+--);
+
+--create table Prijava(
+--studentID int,
+--ispitniRokID int,
+--pristupio bit
+--);

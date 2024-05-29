2f6591f25b6a98d65609134e3baf938e6652d8a4
Petar9512 <petarjelicic95@gmail.com>
2024-05-30 01:36:32 +0200

    kreiranje baze random

--- vjezba.sql
+++ vjezba.sql
@@ -1,67 +1,85 @@
---create database vjezba;
+use master;
+go
+drop database if exists random;
+go
+create database random;
+go
+use random;
 
---create table IspitniRok(
---sifra int NOT NULL,
---predmet varchar(50) NOT NULL,
---vrstaIspita varchar(50) NOT NULL,
---datum datetime,
---pristupio bit NOT NULL
---);
+create table IspitniRok(
+sifra int not null primary key identity(1,1),
+predmet varchar(50) not null,
+vrstaIspita varchar(50) not null,
+datum datetime,
+pristupio bit not null
+);
 
---create table Pristupnici(
---ispitniRok int NOT NULL,
---student varchar(50) NOT NULL,
---brojBodova int NOT NULL,
---ocjena int NOT NULL
---);
+create table Pristupnici(
+ispitniRok int not null,
+student varchar(50) not null,
+brojBodova int not null,
+ocjena int not null
+);
 
---create table odjel(
---sifra int,
---naziv varchar(45)
---);
+create table odjel(
+sifra int not null primary key identity(1,1),
+naziv varchar(45) not null
+);
 
---create table zaposlenik(
---sifra int,
---nadredeni int,
---ime varchar(45),
---prezime varchar(45),
---datumrodenja datetime,
---placa decimal(18,2),
---odjel int
---);
+create table zaposlenik(
+sifra int not null primary key identity(1,1),
+nadredeni int,
+ime varchar(45) not null,
+prezime varchar(45) not null,
+datumrodenja datetime not null,
+placa decimal(18,2),
+odjel int not null
+);
 
---create table projekt(
---sifra int,
---naziv varchar(45),
---datumpocetka datetime,
---datumkraja datetime
---);
+create table projekt(
+sifra int not null primary key identity(1,1),
+naziv varchar(45) not null,
+datumpocetka datetime,
+datumkraja datetime
+);
 
---create table sudjeluje(
---zaposlenik int,
---projekt int,
---datumpristupanja datetime
---);
+create table sudjeluje(
+zaposlenik int not null,
+projekt int not null,
+datumpristupanja datetime
+);
 
---create table Racun(
---sifra int NOT NULL,
---redniBroj int NOT NULL,
---datum datetime,
---kupac varchar(50) NOT NULL,
---placeno bit NOT NULL
---);
+create table Racun(
+sifra int not null primary key identity(1,1),
+redniBroj int not null,
+datum datetime,
+kupac varchar(50) not null,
+placeno bit not null
+);
 
---create table StavkeRacuna(
---racun int NOT NULL,
---artikl varchar(50) NOT NULL,
---cijena decimal(18,2) NOT NULL,
---kolicina varchar(20) NOT NULL,
---popust varchar(20) NOT NULL
---);
+create table StavkeRacuna(
+racun int not null,
+artikl varchar(50) not null,
+cijena decimal(18,2) not null,
+kolicina varchar(20) not null,
+popust varchar(20)
+);
 
---create table radnik(
---sifra int NOT NULL,
---Ime nvarchar(50) NOT NULL,
---Prezime nvarchar(50) NOT NULL,
---nadredeni int NOT NULL
---);
+create table radnik(
+sifra int not null primary key identity(1,1),
+Ime nvarchar(50) not null,
+Prezime nvarchar(50) not null,
+nadredeni int
+);
+
+
+alter table Pristupnici add foreign key (ispitniRok) references IspitniRok(sifra);
+
+alter table zaposlenik add foreign key (odjel) references odjel(sifra);
+alter table zaposlenik add foreign key (nadredeni) references zaposlenik(sifra);
+alter table sudjeluje add foreign key (zaposlenik) references zaposlenik(sifra);
+alter table sudjeluje add foreign key (projekt) references projekt(sifra);
+
+alter table StavkeRacuna add foreign key (racun) references Racun(sifra);
+
+alter table radnik add foreign key (nadredeni) references radnik(sifra);

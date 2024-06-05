ff7e67a3bdb8ab29d127e2e5bdf12afa22a4c5d1
Petar9512 <petarjelicic95@gmail.com>
2024-06-05 16:08:55 +0200

    dodavanje vrijednosti u tablice i promjena datetime u date

--- random.sql
+++ random.sql
@@ -10,8 +10,7 @@ create table IspitniRok(
 sifra int not null primary key identity(1,1),
 predmet varchar(50) not null,
 vrstaIspita varchar(50) not null,
-datum datetime,
-pristupio bit not null
+datum datetime
 );
 
 create table Pristupnici(
@@ -31,7 +30,7 @@ sifra int not null primary key identity(1,1),
 nadredeni int,
 ime varchar(45) not null,
 prezime varchar(45) not null,
-datumrodenja datetime not null,
+datumrodenja date not null,
 placa decimal(18,2),
 odjel int not null
 );
@@ -39,14 +38,14 @@ odjel int not null
 create table projekt(
 sifra int not null primary key identity(1,1),
 naziv varchar(45) not null,
-datumpocetka datetime,
-datumkraja datetime
+datumpocetka date,
+datumkraja date
 );
 
 create table sudjeluje(
 zaposlenik int not null,
 projekt int not null,
-datumpristupanja datetime
+datumpristupanja date
 );
 
 create table Racun(
@@ -83,3 +82,70 @@ alter table sudjeluje add foreign key (projekt) references projekt(sifra);
 alter table StavkeRacuna add foreign key (racun) references Racun(sifra);
 
 alter table radnik add foreign key (nadredeni) references radnik(sifra);
+
+
+insert into IspitniRok (predmet,vrstaIspita,datum) values
+('matematika','pismeni','2024-03-01 10:00'),
+('fizika','pismeni','2024-03-03 12:00'),
+('kemija','pismeni','2024-03-07 10:00'),
+('biologija','pismeni','2024-03-09 9:00'),
+('logika','pismeni','2024-03-11 9:00');
+
+insert into Pristupnici (ispitniRok,student,brojBodova,ocjena) values
+(1,'Ivan Iviæ',46,5),
+(3,'Hajdi Hajdiæ',42,4),
+(4,'Pero Periæ',20,3),
+(2,'Ivan Ivièeviæ',33,3),
+(5,'Goran Grgiæ', 16,2);
+
+
+
+insert into odjel (naziv) values
+('Kemija'),
+('Fizika'),
+('Matematika'),
+('Logika'),
+('Biologija');
+
+insert into zaposlenik (nadredeni,ime,prezime,datumrodenja,placa,odjel) values
+(3,'Pero','Periæ','1980-02-03',null,1),
+(null,'Hajdi','Hajdiæ','1984-03-17',750.75,5),
+(null,'Ivan','Ivièeviæ','1977-11-05',null,1),
+(2,'Goran','Grgiæ','1988-01-10',650.25,5),
+(3,'Ivan','Iviæ','1987-07-05',680.89,1);
+
+insert into projekt (naziv,datumpocetka,datumkraja) values
+('Predavanja u OŠ','2024-04-16','2024-04-23'),
+('Seminari fakultet','2024-03-27','2024-04-10'),
+('Istraživanje kakvoæe mora','2024-06-05','2024-06-10'),
+('Ljetni kamp','2024-07-16','2024-07-30'),
+('Mikroskopska analiza','2024-05-10','2024-05-11');
+
+insert into sudjeluje (zaposlenik,projekt,datumpristupanja) values
+(2,1,null), (4,1,null), (1,2,null), (3,2,null), (2,3,null), (4,3,null), (3,4,null), (5,4,'2024-07-23'), (2,5,'2024-05-10');
+
+
+
+insert into Racun (redniBroj,datum,kupac,placeno) values
+(1,'2024-06-01 08:02','Ivan Iviæ',1),
+(2,'2024-06-01 08:33','Hajdi Hajdiæ',1),
+(3,'2024-06-01 08:42','Pero Periæ',1),
+(4,'2024-06-01 09:07','Goran Grgiæ',1),
+(5,'2024-06-01 09:32','Ivan Ivièeviæ',1);
+
+insert into StavkeRacuna (racun,artikl,cijena,kolicina,popust) values
+(2,'Banane',2.25,'7',null),
+(2,'Naranèe',1.32,'5',null),
+(1,'Èokolada Dorina',3.98,'2',null),
+(1,'Èips Pringles',3.25,'1',null),
+(1,'Jabuke',1.77,'5', null);
+
+
+
+insert into radnik (Ime,Prezime,nadredeni) values
+('Hajdi','Hajdiæ', null),
+('Ivan','Ivièeviæ',null),
+('Pero','Periæ',2),
+('Ivan','Iviæ',2),
+('Goran','Grgiæ',1);
+

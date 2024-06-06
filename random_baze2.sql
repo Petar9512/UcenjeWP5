256722f1632d50f6544418229ba3cabba4bd3c86
Petar9512 <petarjelicic95@gmail.com>
2024-06-06 03:49:54 +0200

    dodavanje vrijednosti u tablice datoteke random_baze2

--- random_baze2.sql
+++ random_baze2.sql
@@ -39,6 +39,33 @@ alter table Posjet add foreign key (djelatnikID) references Djelatnici(djelatnik
 alter table Posjet add foreign key (korisnikID) references Korisnici(korisnikID);
 alter table Posjet add foreign key (uslugaID) references Usluge(uslugaID);
 
+insert into Djelatnici (ime,prezime,brojTelefona) values
+('Hajdi','Hajdiæ','0984447773'),
+('Ivan','Iviæ',null),
+('Goran','Grkiæ',null),
+('Ivan','Ivièeviæ','0917777788'),
+('Pero','Periæ',null);
+
+insert into Korisnici (ime,prezime,spol,tipKose) values
+('Sonja','Sonjiæ','žena','dugaèka'),
+('Marko','Markiæ','muškarac','kratka'),
+('Ivana','Iviæ','žena','valovita'),
+('Petra','Periæ','žena','kovrèava'),
+('Ivana','Ivièeviæ','žena','dugaèka');
+
+insert into Usluge (naziv,opis,cijena) values
+('šišanje',null,7.50),
+('pranje kose i šišanje',null,8.50),
+('pranje kose, šišanje i feniranje',null,9.50);
+
+insert into Posjet (djelatnikID,korisnikID,uslugaID,vrijeme) values
+(1,3,3,'2024-05-10 11:00'),
+(4,2,1,'2024-05-10 11:33'),
+(5,5,3,'2024-05-20 10:48'),
+(4,1,1,null),
+(1,3,3,'2024-07-16 9:55');
+
+
 
 create table Zaposlenici(
 zaposlenikID int not null primary key identity(1,1),
@@ -68,11 +95,31 @@ sirina varchar(20)
 alter table Sticenici add foreign key (zaposlenikID) references Zaposlenici(zaposlenikID);
 alter table Sticenici add foreign key (prostorID) references Prostori(prostorID);
 
+insert into Zaposlenici (ime,prezime,brojTelefona,email) values
+('Pero','Periæ','0915555558','peroperic@gmail.com'),
+('Ivan','Ivièeviæ',null,null),
+('Goran','Grgiæ','0987775559','gorangrgic@gmail.com'),
+('Ivan','Iviæ',null,null),
+('Hajdi','Hajdiæ',null,null);
+
+insert into Prostori (tip,visina,duzina,sirina) values
+('kavez','1m','2m','50cm'),
+('krletka','40cm','40cm','40cm'),
+('terarij',null,null,null),
+('akvarij',null,null,null);
+
+insert into Sticenici (zaposlenikID,prostorID,ime,vrsta,prehrana) values
+(4,4,'Nemo','riba','riblji kroketi'),
+(2,3,null,'zmija','bilošto'),
+(5,2,'Nevis','papiga','sjemenke'),
+(3,1,null,'majmun','voæe');
+
+
 
 create table OdgojneSkupine(
 odgojnaSkupinaID int not null primary key identity(1,1),
 odgajateljicaID int not null,
-naziv varchar(10) not null,
+naziv varchar(30) not null,
 brojDjece int
 );
 
@@ -101,6 +148,27 @@ opis varchar(50) not null
 alter table OdgojneSkupine add foreign key (odgajateljicaID) references Odgajateljice(odgajateljicaID);
 alter table Djeca add foreign key (odgojnaSkupinaID) references OdgojneSkupine(odgojnaSkupinaID);
 alter table Odgajateljice add foreign key (strucnaSpremaID) references StrucnaSprema(strucnaSpremaID);
+
+insert into StrucnaSprema (naziv,opis) values
+('Uèiteljski fakultet','završila uèiteljski fakultet'),
+('Odgojno-obrazovni teèaj','završila odgojno-obrazovni teèaj');
+
+insert into Odgajateljice (ime,prezime,brojTelefona,strucnaSpremaID) values
+('Hajdi','Hajdiæ','0987775558',1),
+('Sonja','Sonjiæ',null,1),
+('Ivana','Iviæ',null,2);
+
+insert into OdgojneSkupine (odgajateljicaID,naziv,brojDjece) values
+(3,'odgojna skupina 1',null),
+(2,'odgojna skupina 2',null),
+(1,'odgojna skupina 3',null);
+
+insert into Djeca (ime,prezime,dob,kontaktRoditelja,odgojnaSkupinaID) values
+('Pero','Periæ',7,'0985557778',3),
+('Ivan','Iviæ',8,'0912224443',3),
+('Ivan','Ivièeviæ',7,'0983334445',3),
+('Marko','Markiæ',8,'0985554446',3);
+
 
 
 create table Izlozbe(
@@ -136,6 +204,27 @@ alter table Djela add foreign key (izlozbaID) references Izlozbe(izlozbaID);
 alter table Kustosi add foreign key (izlozbaID) references Izlozbe(izlozbaID);
 alter table Izlozbe add foreign key (sponzorID) references Sponzori(sponzorID);
 
+insert into Sponzori (naziv,kontakt,donacija) values
+('Microsoft',null,null),
+('Facebook',null,null),
+('Amazon',null,null);
+
+insert into Izlozbe (naziv,brojDjela,vrijemePocetka,sponzorID) values
+('Renesansna izložba',7,'2024-08-01 10:00',2),
+('Srednjovjekovna izložba',5,'2024-08-08 12:00',3),
+('Futuristièka izložba',7,'2024-08-15 12:00',1);
+
+insert into Kustosi (ime,prezime,brojTelefona,izlozbaID) values
+('Ivan','Iviæ','0985557778',3),
+('Ivan','Ivièeviæ','091555774',3),
+('Hajdi','Hajdiæ','0987774445',1);
+
+insert into Djela (naziv,autor,stil,izlozbaID) values
+('Mona Lisa','Leonardo da Vinci',null,1),
+('Futurizam 2050','Pero Periæ',null,3),
+('Srednji vijek 1000','Goran Grkiæ',null,2);
+
+
 
 create table Svecenici(
 svecenikID int not null primary key identity(1,1),
@@ -147,8 +236,8 @@ nadredeni int
 create table Poslovi(
 posaoID int not null primary key identity(1,1),
 opis varchar(100) not null,
-vrijemePocetka datetime,
-vrijemeZavrsetka datetime
+vrijemePocetka time,
+vrijemeZavrsetka time
 );
 
 create table Raspored(
@@ -160,6 +249,25 @@ vrijeme datetime not null
 alter table Svecenici add foreign key (nadredeni) references Svecenici(svecenikID);
 alter table Raspored add foreign key (svecenikID) references Svecenici(svecenikID);
 alter table Raspored add foreign key (posaoID) references Poslovi(posaoID);
+
+insert into Svecenici (ime,dob,nadredeni) values
+('Ivan',48,null),
+('Petar',38,null),
+('Matej',36,1),
+('Marko',33,1);
+
+insert into Poslovi (opis,vrijemePocetka,vrijemeZavrsetka) values
+('Kuhanje svaki dan','08:00','12:00'),
+('Èišæenje svaki dan','18:00','20:00'),
+('Vrtlarenje pon-pet','14:00','16:00');
+
+insert into Raspored (svecenikID,posaoID,vrijeme) values
+(2,1,'2024-06-01 09:30'),
+(2,2,'2024-06-01 19:00'),
+(1,1,'2024-06-02 08:30'),
+(3,3,'2024-06-02 14:00'),
+(4,2,'2024-06-02 18:00');
+
 
 
 create table Vozila(

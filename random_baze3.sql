a423aaade686b3fb06efdf5dea99f187dad66207
Petar9512 <petarjelicic95@gmail.com>
2024-06-06 18:38:04 +0200

    update random_baze3

--- random_baze3.sql
+++ random_baze3.sql
@@ -41,6 +41,31 @@ alter table Popravci add foreign key (korisnikID) references Korisnici(korisnikI
 alter table PopravakObuce add foreign key (popravakID) references Popravci(popravakID);
 alter table PopravakObuce add foreign key (obucaID) references Obuca(obucaID);
 
+insert into Korisnici (ime,prezime,kontakt) values
+('Ivan','Iviæ','0987775556'),
+('Goran','Grgiæ','0957778887'),
+('Hajdi','Hajdiæ','0913332224'),
+('Ivan','Ivièeviæ','0914443335');
+
+insert into Obuca (korisnikID,vrsta,broj,opisKvara) values
+(4,'jedan par cipela','42','probušeno dno'),
+(4,'jedan par sandala','42',null),
+(3,'desna èizma','30',null),
+(2,'jedan par cipela','43',null),
+(2,'jedan par papuèa','43',null);
+
+insert into Popravci (korisnikID,kolicinaObuce) values
+(4,'jedan par cipela i jedan par sandala'),
+(2,'jedan par cipela i jedan par papuèa'),
+(3,'desna èizma');
+
+insert into PopravakObuce (popravakID,obucaID,vrijemePopravka,opisPopravka,cijena,segrt) values
+(1,2,'2024-06-01 10:00','popravio desnu sandalu',5.50,0),
+(1,2,'2024-06-01 17:25','popravio lijevu sandalu',5.50,0),
+(2,5,'2024-06-03 9:00','popravio papuèe',10.00,1),
+(2,4,'2024-06-04 10:20','popravio cipele',12.00,1);
+
+
 
 create table Pacijenti(
 pacijentID int not null primary key identity(1,1),
@@ -62,14 +87,14 @@ medicinskeSestre varchar(100)
 alter table Lijecenje add foreign key (pacijentID) references Pacijenti(pacijentID);
 
 insert into Pacijenti (ime,prezime,dob,opisBolesti) values
-('Ivan','Iviï¿½eviï¿½',33,'prehlada'),
-('Goran','Grkiï¿½',43,'upala grla'),
-('Marko','Markiï¿½',38,'gastritis'),
-('Ivan','Iviï¿½eviï¿½',33,'gastritis');
+('Ivan','Ivièeviæ',33,'prehlada'),
+('Goran','Grkiæ',43,'upala grla'),
+('Marko','Markiæ',38,'gastritis'),
+('Ivan','Ivièeviæ',33,'gastritis');
 
 insert into Lijecenje (pacijentID,opisTretmana,lijekovi,pocetakLijecenja,krajLijecenja,medicinskeSestre) values
-(1,'sirup za kaï¿½alj',null,'2024-03-01','2024-03-08',null),
-(2,'sirup za kaï¿½alj',null,'2024-03-20','2024-03-25',null),
+(1,'sirup za kašalj',null,'2024-03-01','2024-03-08',null),
+(2,'sirup za kašalj',null,'2024-03-20','2024-03-25',null),
 (3,'antibiotik',null,'2024-04-10','2024-04-15',null),
 (4,'antibiotik',null,'2024-05-22','2024-05-29',null);
 
@@ -101,16 +126,16 @@ alter table Prijava add foreign key (studentID) references Studenti(studentID);
 alter table Prijava add foreign key (ispitniRokID) references IspitniRokovi(ispitniRokID);
 
 insert into Studenti (ime,prezime,oib,datumRodenja,smjer) values
-('Marko','Markiï¿½',null,null,'Germanistika'),
-('Hajdi','Hajdiï¿½',null,null,'Kroatistika'),
-('Ivan','Iviï¿½',null,null,'Talijanistika');
+('Marko','Markiæ',null,null,'Germanistika'),
+('Hajdi','Hajdiæ',null,null,'Kroatistika'),
+('Ivan','Iviæ',null,null,'Talijanistika');
 
 insert into IspitniRokovi (kolegij,vrstaIspita,vrijeme) values
-('Jeziï¿½ne vjeï¿½be 1','pismeni','2024-07-07 10:00'),
+('Jeziène vježbe 1','pismeni','2024-07-07 10:00'),
 ('Uvod u lingvistiku','pismeni','2024-06-28 13:00'),
 ('Staroslavenski jezik','pismeni','2024-06-30 12:00'),
 ('Fonetika','usmeni','2024-07-02 10:00'),
-('Jeziï¿½ne vjeï¿½be 2','pismeni','2024-07-07 14:00');
+('Jeziène vježbe 2','pismeni','2024-07-07 14:00');
 
 insert into Prijava (studentID,ispitniRokID,pristupio) values
 (2,3,1),(2,1,1),(1,2,0),(3,5,1);

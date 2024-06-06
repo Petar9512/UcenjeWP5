c112d3a0d087fe9b56f85b85d983d39561aa67ce
Petar9512 <petarjelicic95@gmail.com>
2024-06-06 18:36:47 +0200

    dodavanje vrijednosti u tablice datoteke random_baze3

--- random_baze3.sql
+++ random_baze3.sql
@@ -16,9 +16,9 @@ kontakt varchar(30) not null
 create table Obuca(
 obucaID int not null primary key identity(1,1),
 korisnikID int not null,
-vrsta varchar(30),
-broj varchar(10),
-opisKvara varchar(100) not null
+vrsta varchar(30) not null,
+broj varchar(10) not null,
+opisKvara varchar(100)
 );
 
 create table Popravci(
@@ -54,12 +54,25 @@ create table Lijecenje(
 pacijentID int not null,
 opisTretmana varchar(300) not null,
 lijekovi varchar(100),
-pocetakLijecenja datetime not null,
-krajLijecenja datetime not null,
+pocetakLijecenja date not null,
+krajLijecenja date not null,
 medicinskeSestre varchar(100)
 );
 
 alter table Lijecenje add foreign key (pacijentID) references Pacijenti(pacijentID);
+
+insert into Pacijenti (ime,prezime,dob,opisBolesti) values
+('Ivan','Ivi�evi�',33,'prehlada'),
+('Goran','Grki�',43,'upala grla'),
+('Marko','Marki�',38,'gastritis'),
+('Ivan','Ivi�evi�',33,'gastritis');
+
+insert into Lijecenje (pacijentID,opisTretmana,lijekovi,pocetakLijecenja,krajLijecenja,medicinskeSestre) values
+(1,'sirup za ka�alj',null,'2024-03-01','2024-03-08',null),
+(2,'sirup za ka�alj',null,'2024-03-20','2024-03-25',null),
+(3,'antibiotik',null,'2024-04-10','2024-04-15',null),
+(4,'antibiotik',null,'2024-05-22','2024-05-29',null);
+
 
 
 create table Studenti(
@@ -67,7 +80,7 @@ studentID int not null primary key identity(1,1),
 ime varchar(30) not null,
 prezime varchar(30) not null,
 oib char(11),
-datumRodenja datetime,
+datumRodenja date,
 smjer varchar(50)
 );
 
@@ -86,6 +99,22 @@ pristupio bit
 
 alter table Prijava add foreign key (studentID) references Studenti(studentID);
 alter table Prijava add foreign key (ispitniRokID) references IspitniRokovi(ispitniRokID);
+
+insert into Studenti (ime,prezime,oib,datumRodenja,smjer) values
+('Marko','Marki�',null,null,'Germanistika'),
+('Hajdi','Hajdi�',null,null,'Kroatistika'),
+('Ivan','Ivi�',null,null,'Talijanistika');
+
+insert into IspitniRokovi (kolegij,vrstaIspita,vrijeme) values
+('Jezi�ne vje�be 1','pismeni','2024-07-07 10:00'),
+('Uvod u lingvistiku','pismeni','2024-06-28 13:00'),
+('Staroslavenski jezik','pismeni','2024-06-30 12:00'),
+('Fonetika','usmeni','2024-07-02 10:00'),
+('Jezi�ne vje�be 2','pismeni','2024-07-07 14:00');
+
+insert into Prijava (studentID,ispitniRokID,pristupio) values
+(2,3,1),(2,1,1),(1,2,0),(3,5,1);
+
 
 
 create table Radionice(

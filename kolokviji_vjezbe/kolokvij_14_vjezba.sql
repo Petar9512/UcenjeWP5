60619d58f6bb0750bf7d544ad73451967278e507
Petar9512 <petarjelicic95@gmail.com>
2024-06-27 17:55:32 +0200

    kolokviji 14-15

---
+++ kolokviji_vjezbe/kolokvij_14_vjezba.sql
@@ -0,0 +1,110 @@
+﻿create database kolokvij_vjezba_14;
+use kolokvij_vjezba_14;
+
+
+create table ostavljena (
+sifra int not null primary key identity(1,1),
+kuna decimal(17,7),
+hlace varchar(45),
+suknja varchar(36),
+dukserica varchar(36),
+kratkamajica varchar(47) not null,
+gustoca decimal(12,10) not null,
+ostavljen int
+);
+
+create table ostavljen (
+sifra int not null primary key identity(1,1),
+majica varchar(39),
+drugiputa datetime not null,
+asocijalno bit not null,
+brat int not null
+);
+
+create table brat (
+sifra int not null primary key identity(1,1),
+eura decimal(15,10) not null,
+gustoca decimal(14,5) not null,
+kuna decimal(15,6),
+haljina varchar(38) not null,
+bojakose varchar(39),
+prstena int,
+neprijatelj int not null
+);
+
+create table neprijatelj (
+sifra int not null primary key identity(1,1),
+prstena int not null,
+gustoca decimal(17,7),
+bojakose varchar(48) not null,
+ogrlica int not null,
+stilfrizura varchar(47) not null
+);
+
+create table muskarac (
+sifra int not null primary key identity(1,1),
+carape varchar(36),
+ogrlica int not null,
+vesta varchar(44) not null,
+ekstroventno bit
+);
+
+create table neprijatelj_muskarac (
+sifra int not null primary key identity(1,1),
+neprijatelj int not null,
+muskarac int not null
+);
+
+--drop table nepriatelj_muskarac;
+
+create table mladic (
+sifra int not null primary key identity(1,1),
+drugiputa datetime not null,
+narukvica int,
+bojaociju varchar(49) not null,
+modelnaocala varchar(41)
+);
+
+create table prijatelj (
+sifra int not null primary key identity(1,1),
+stilfrizura varchar(39) not null,
+kratkamajica varchar(32),
+treciputa datetime,
+modelnaocala varchar(32) not null,
+prviputa datetime,
+mladic int not null
+);
+
+alter table ostavljena add foreign key (ostavljen) references ostavljen(sifra);
+alter table ostavljen add foreign key (brat) references brat(sifra);
+alter table brat add foreign key (neprijatelj) references neprijatelj(sifra);
+alter table neprijatelj_muskarac add foreign key (neprijatelj) references neprijatelj(sifra);
+alter table neprijatelj_muskarac add foreign key (muskarac) references muskarac(sifra);
+alter table prijatelj add foreign key (mladic) references mladic(sifra);
+
+
+insert into neprijatelj (prstena,bojakose,ogrlica,stilfrizura) values
+(2,'crna',1,'kratka'),(1,'smeđa',0,'ananas'),(0,'crna',1,'banana');
+insert into muskarac (ogrlica,vesta) values
+(2,'plava'),(1,'žuta');
+insert into neprijatelj_muskarac (neprijatelj,muskarac) values
+(1,1),(1,2),(2,2);
+insert into brat (eura,gustoca,haljina,neprijatelj) values
+(34.67,12.52,'zelena',2),(4.87,35.91,'narančasta',2),(25.75,15.98,'crvena',3);
+insert into ostavljen (drugiputa,asocijalno,brat) values
+('2024-03-21',0,1),('2024-02-26',0,2),('2024-01-30',1,2);
+
+update prijatelj set kratkamajica='Osijek';
+delete from ostavljena where hlace !='AB';
+select kuna from brat where gustoca not in (6,10,16,25,36);
+
+select muskarac.vesta, ostavljena.dukserica, ostavljen.drugiputa
+from muskarac inner join neprijatelj_muskarac on muskarac.sifra=neprijatelj_muskarac.muskarac
+inner join neprijatelj on neprijatelj_muskarac.neprijatelj=neprijatelj.sifra
+inner join brat on neprijatelj.sifra=brat.neprijatelj
+inner join ostavljen on brat.sifra=ostavljen.brat
+inner join ostavljena on ostavljen.sifra=ostavljena.ostavljen
+where brat.gustoca > 100 and neprijatelj.gustoca !=22
+order by ostavljen.drugiputa desc;
+
+select gustoca, bojakose from neprijatelj where sifra not in (select neprijatelj from neprijatelj_muskarac);
---
+++ kolokviji_vjezbe/kolokvij_15_vjezba.sql
@@ -0,0 +1,108 @@
+﻿create database kolokvij_vjezba_15;
+use kolokvij_vjezba_15;
+
+
+create table decko (
+sifra int not null primary key identity(1,1),
+vesta varchar(37) not null,
+bojakose varchar(45),
+gustoca decimal(18,7),
+prijatelj int not null
+);
+
+create table prijatelj (
+sifra int not null primary key identity(1,1),
+majica varchar(41) not null,
+vesta varchar(30),
+narukvica int not null,
+zarucnica int
+);
+
+create table zarucnica (
+sifra int not null primary key identity(1,1),
+prviputa datetime not null,
+bojaociju varchar(31) not null,
+modelnaocala varchar(40),
+zarucnik int
+);
+
+create table zarucnik (
+sifra int not null primary key identity(1,1),
+kratkamajica varchar(30) not null,
+jmbag char(11) not null,
+dukserica varchar(45) not null,
+indiferentno bit,
+treciputa datetime not null
+);
+
+create table ostavljena (
+sifra int not null primary key identity(1,1),
+gustoca decimal(16,7),
+stilfrizura varchar(31) not null,
+ogrlica int,
+maraka decimal(15,10)
+);
+
+create table zarucnik_ostavljena (
+sifra int not null primary key identity(1,1),
+zarucnik int not null,
+ostavljena int not null
+);
+
+--drop table zarucnik_ostavljena;
+
+create table brat (
+sifra int not null primary key identity(1,1),
+bojakose varchar(49) not null,
+majica varchar(36),
+maraka decimal(17,9),
+vesta varchar(30) not null
+);
+
+create table prijateljica (
+sifra int not null primary key identity(1,1),
+kratkamajica varchar(30) not null,
+bojakose varchar(45),
+asocijalno bit not null,
+treciputa datetime not null,
+jmbag char(11) not null,
+vesta varchar(42) not null,
+brat int
+);
+
+alter table decko add foreign key (prijatelj) references prijatelj(sifra);
+alter table prijatelj add foreign key (zarucnica) references zarucnica(sifra);
+alter table zarucnica add foreign key (zarucnik) references zarucnik(sifra);
+alter table zarucnik_ostavljena add foreign key (zarucnik) references zarucnik(sifra);
+alter table zarucnik_ostavljena add foreign key (ostavljena) references ostavljena(sifra);
+alter table prijateljica add foreign key (brat) references brat(sifra);
+
+
+insert into zarucnik (kratkamajica,jmbag,dukserica,treciputa) values
+('plava','34657895846','ananas','2024-03-29'),('narančasta','77834567254','banana','2024-01-09'),
+('zelena','24759874691','avokado','2024-02-12');
+insert into ostavljena (stilfrizura) values
+('kratka'),('valovita');
+insert into zarucnik_ostavljena (zarucnik,ostavljena) values
+(1,1),(1,2),(2,2);
+insert into zarucnica (prviputa,bojaociju) values
+('2024-04-28','plava'),('2024-01-11','crna'),('2024-03-03','smeđa');
+insert into prijatelj (majica,narukvica) values
+('ananas',2),('avokado',1),('banana',2);
+
+update prijateljica set bojakose='Osijek';
+delete from decko where bojakose='AB';
+select modelnaocala from zarucnica where bojaociju like '%ana%';
+
+select ostavljena.ogrlica, decko.prijatelj, prijatelj.vesta 
+from ostavljena inner join zarucnik_ostavljena on ostavljena.sifra=zarucnik_ostavljena.ostavljena
+inner join zarucnik on zarucnik_ostavljena.zarucnik=zarucnik.sifra
+inner join zarucnica on zarucnik.sifra=zarucnica.zarucnik
+inner join prijatelj on zarucnica.sifra=prijatelj.zarucnica
+inner join decko on prijatelj.sifra=decko.prijatelj
+where zarucnica.bojaociju like 'a%' and zarucnik.jmbag is not null
+order by prijatelj.vesta desc;
+
+select zarucnik.jmbag, zarucnik.dukserica
+from zarucnik left join zarucnik_ostavljena on zarucnik.sifra=zarucnik_ostavljena.zarucnik
+where zarucnik_ostavljena.zarucnik is null;

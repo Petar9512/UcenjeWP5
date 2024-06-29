0733d17f023d3c6fed922175b87ecc48dbd9ac8b
Petar9512 <petarjelicic95@gmail.com>
2024-06-29 23:54:44 +0200

    kolokviji 19/20

---
+++ kolokviji_vjezbe/kolokvij_19_vjezba.sql
@@ -0,0 +1,107 @@
+﻿create database kolokvij_vjezba_19;
+use kolokvij_vjezba_19;
+
+
+create table muskarac (
+sifra int not null primary key identity(1,1),
+drugiputa datetime not null,
+gustoca decimal(16,7),
+maraka decimal(16,9),
+ogrlica int not null,
+svekrva int
+);
+
+create table svekrva (
+sifra int not null primary key identity(1,1),
+ekstroventno bit,
+carape varchar(42),
+gustoca decimal(13,10) not null,
+stilfrizura varchar(34) not null,
+punac int not null
+);
+
+create table punac (
+sifra int not null primary key identity(1,1),
+maraka decimal(12,7),
+treciputa datetime,
+ekstroventno bit,
+hlace varchar(32),
+punica int not null
+);
+
+create table punica (
+sifra int not null primary key identity(1,1),
+stilfrizura varchar(39),
+maraka decimal(15,6) not null,
+jmbag char(11)
+);
+
+create table ostavljena (
+sifra int not null primary key identity(1,1),
+hlace varchar(41),
+ekstroventno bit not null,
+narukvica int,
+eura decimal(17,6) not null,
+kratkamajica varchar(46)
+);
+
+create table punica_ostavljena (
+sifra int not null primary key identity(1,1),
+punica int not null,
+ostavljena int not null
+);
+
+create table decko (
+sifra int not null primary key identity(1,1),
+modelnaocala varchar(42) not null,
+gustoca decimal(16,10) not null,
+dukserica varchar(47) not null,
+stilfrizura varchar(40),
+novcica decimal(14,7) not null
+);
+
+create table svekar (
+sifra int not null primary key identity(1,1),
+nausnica int not null,
+indiferentno bit not null,
+suknja varchar(37) not null,
+drugiputa datetime,
+kuna decimal(13,6) not null,
+decko int
+);
+
+alter table svekar add foreign key (decko) references decko(sifra);
+alter table muskarac add foreign key (svekrva) references svekrva(sifra);
+alter table svekrva add foreign key (punac) references punac(sifra);
+alter table punac add foreign key (punica) references punica(sifra);
+alter table punica_ostavljena add foreign key (punica) references punica(sifra);
+alter table punica_ostavljena add foreign key (ostavljena) references ostavljena(sifra);
+
+
+insert into punica (stilfrizura,maraka,jmbag) values
+('kratka',23.86,'08755668451'),('dugačka',12.98,'47689228657'),('valovita',33.88,'43598182783');
+insert into ostavljena (ekstroventno,eura) values
+(1,32.64),(0,12.72);
+insert into punica_ostavljena (punica,ostavljena) values
+(1,2),(2,1),(2,2);
+insert into punac (punica) values
+(2),(3),(3);
+insert into svekrva (gustoca,stilfrizura,punac) values
+(33.42,'kratka',1),(35.11,'dugačka',3),(26.72,'valovita',3);
+
+update svekar set indiferentno=0;
+delete from muskarac where gustoca=14.92;
+select ekstroventno from punac where treciputa is null;
+
+select ostavljena.narukvica, muskarac.ogrlica, svekrva.carape
+from ostavljena inner join punica_ostavljena on ostavljena.sifra=punica_ostavljena.ostavljena
+inner join punica on punica_ostavljena.punica=punica.sifra
+inner join punac on punica.sifra=punac.punica
+inner join svekrva on punac.sifra=svekrva.punac
+inner join muskarac on svekrva.sifra=muskarac.svekrva
+where punac.treciputa is not null and punica.maraka !=21
+order by svekrva.carape desc;
+
+select punica.maraka, punica.jmbag 
+from punica left join punica_ostavljena on punica.sifra=punica_ostavljena.punica
+where punica_ostavljena.punica is null;
---
+++ kolokviji_vjezbe/kolokvij_vjezba_20.sql
@@ -0,0 +1,109 @@
+﻿create database kolokvij_vjezba_20;
+use kolokvij_vjezba_20;
+
+
+create table neprijateljica (
+sifra int not null primary key identity(1,1),
+introvertno bit not null,
+nausnica int,
+gustoca decimal(17,8),
+lipa decimal(17,8),
+jmbag char(11) not null,
+zena int
+);
+
+create table zena (
+sifra int not null primary key identity(1,1),
+asocijalno bit not null,
+nausnica int,
+jmbag char(11) not null,
+bojaociju varchar(41),
+kratkamajica varchar(30)
+);
+
+create table zarucnica (
+sifra int not null primary key identity(1,1),
+modelnaocala varchar(50),
+suknja varchar(49) not null,
+prviputa datetime,
+treciputa datetime,
+decko int
+);
+
+create table decko (
+sifra int not null primary key identity(1,1),
+kuna decimal(14,5),
+novcica decimal(15,7),
+indiferentno bit not null,
+maraka decimal(16,9) not null,
+punica int not null
+);
+
+create table punica (
+sifra int not null primary key identity(1,1),
+ogrlica int,
+prviputa datetime,
+drugiputa datetime not null,
+introvertno bit,
+treciputa datetime,
+mladic int not null
+);
+
+create table mladic (
+sifra int not null primary key identity(1,1),
+hlace varchar(47),
+suknja varchar(50),
+haljina varchar(50) not null,
+prstena int not null,
+treciputa datetime not null,
+introvertno bit not null
+);
+
+create table zarucnik (
+sifra int not null primary key identity(1,1),
+suknja varchar(31),
+prstena int not null,
+modelnaocala varchar(46) not null,
+novcica decimal(17,5) not null,
+gustoca decimal(17,8)
+);
+
+create table mladic_zarucnik (
+sifra int not null primary key identity(1,1),
+mladic int not null,
+zarucnik int not null
+);
+
+alter table neprijateljica add foreign key (zena) references zena(sifra);
+alter table zarucnica add foreign key (decko) references decko(sifra);
+alter table decko add foreign key (punica) references punica(sifra);
+alter table punica add foreign key (mladic) references mladic(sifra);
+alter table mladic_zarucnik add foreign key (mladic) references mladic(sifra);
+alter table mladic_zarucnik add foreign key (zarucnik) references zarucnik(sifra);
+
+
+insert into mladic (haljina,prstena,treciputa,introvertno) values
+('ananas',2,'2024-03-23',0),('banana',1,'2024-05-10',0),('avokado',0,'2024-01-28',1);
+insert into zarucnik (prstena,modelnaocala,novcica) values
+(1,'banana',24.97),(0,'ananas',16.54);
+insert into mladic_zarucnik (mladic,zarucnik) values
+(1,1),(2,1),(2,2);
+insert into punica (drugiputa,mladic) values
+('2024-01-10',2),('2024-02-18',3),('2024-04-21',3);
+insert into decko (indiferentno,maraka,punica) values
+(0,28.81,2),(0,33.41,2),(1,75.23,3);
+
+update neprijateljica set nausnica=10;
+delete from zarucnica where suknja < 'AB';
+select drugiputa from punica where prviputa is null;
+
+select zarucnik.modelnaocala, zarucnica.treciputa, decko.novcica
+from zarucnik inner join mladic_zarucnik on zarucnik.sifra=mladic_zarucnik.zarucnik
+inner join mladic on mladic_zarucnik.mladic=mladic.sifra
+inner join punica on mladic.sifra=punica.mladic
+inner join decko on punica.sifra=decko.punica
+inner join zarucnica on decko.sifra=zarucnica.decko
+where punica.prviputa is not null and mladic.suknja like '%ba%'
+order by decko.novcica desc;
+
+select suknja, haljina from mladic where sifra not in (select mladic from mladic_zarucnik);

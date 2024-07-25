30f1c0283ea32c4caaa00aad3da52e3b5a59e6b4
Petar9512 <petarjelicic95@gmail.com>
2024-07-25 15:54:33 +0200

    napredak za prvo zbrajanje

--- CSHARP/Ucenje/UcenjeCS/LjubavniKalkulator.cs
+++ CSHARP/Ucenje/UcenjeCS/LjubavniKalkulator.cs
@@ -12,8 +12,10 @@ namespace UcenjeCS
         public static void Kalkulator(string ime1, string ime2)
         {
             string ime = ime1 + ime2;
+            string brojSlova = "";
             int[] brojeviSlova = new int[ime.Length];
             int k = 0;
+            
 
 
             for (int i = 0; i < ime.Length; i++)
@@ -22,18 +24,38 @@ namespace UcenjeCS
                 {
                     if (ime[i] == ime[j])
                     {
-                        k++;
-                    }
-                        brojeviSlova[i] = k;
-                                        
+                        k++;                        
+                    }                                                                
                 }
+                brojeviSlova[i] = k;
+                brojSlova += k;
                 k = 0;
             }
 
-            for (int i = 0; i < brojeviSlova.Length; i++)
+            int duljinaBrojaSlova = brojSlova.Length;
+
+                        
+
+            for (int i = 0; i < duljinaBrojaSlova / 2; i++)
             {
-                Console.WriteLine(brojeviSlova[i]);
+                int b = brojeviSlova[i] + brojeviSlova[duljinaBrojaSlova - 1 - i];
+                brojSlova += b;
             }
+
+            if (duljinaBrojaSlova % 2 == 1)
+            {
+                char ostatak = brojSlova[duljinaBrojaSlova / 2];
+                brojSlova = brojSlova.Substring(duljinaBrojaSlova);
+                brojSlova += ostatak;
+            }
+
+            else if (duljinaBrojaSlova % 2 == 0)
+            {
+                brojSlova = brojSlova.Substring(duljinaBrojaSlova);
+            }
+            
+            Console.WriteLine(brojSlova);
+
         }
 
     }

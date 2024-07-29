53d364d76e7bd6f4ef0d44e51f62622ec50200bb
Petar9512 <petarjelicic95@gmail.com>
2024-07-29 20:37:51 +0200

    promjena funkcije

--- CSHARP/Ucenje/UcenjeCS/GeneratorLozinki.cs
+++ CSHARP/Ucenje/UcenjeCS/GeneratorLozinki.cs
@@ -17,7 +17,10 @@ namespace UcenjeCS
             int x;
             int y;
 
-            int[] interpunkZnakovi = new int[] { 33, 44, 46, 58, 63, 64 };
+            int[] velikaSlova = new int[] { 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90 };
+            int[] malaSlova = new int[] { 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122 };
+            int[] brojevi = new int[] { 48, 49, 50, 51, 52, 53, 54, 55, 56, 57 };
+            int[] znakovi = new int[] { 33, 44, 46, 58, 63, 64 };
 
             
             for (int i = 0; i < duljina; i++)
@@ -26,27 +29,27 @@ namespace UcenjeCS
 
                 if (y == 1)
                 {
-                    x = rnd.Next(65, 91);
-                    lozinka += (Char)x;
+                    x = rnd.Next(0, velikaSlova.Length);
+                    lozinka += (Char)velikaSlova[x];
                 }
 
                 else if (y == 2)
                 {
-                    x = rnd.Next(97, 123);
-                    lozinka += (Char)x;
+                    x = rnd.Next(0, malaSlova.Length);
+                    lozinka += (Char)malaSlova[x];
                 }
 
                 else if (y == 3)
                 {
-                    x = rnd.Next(48, 58);
-                    lozinka += (Char)x;
+                    x = rnd.Next(0, brojevi.Length);
+                    lozinka += (Char)brojevi[x];
                                    
                 }
 
                 else if (y == 4)
                 {
-                    x = rnd.Next(0, 6);
-                    lozinka += (Char)interpunkZnakovi[x];
+                    x = rnd.Next(0, znakovi.Length);
+                    lozinka += (Char)znakovi[x];
                 }
             }
 

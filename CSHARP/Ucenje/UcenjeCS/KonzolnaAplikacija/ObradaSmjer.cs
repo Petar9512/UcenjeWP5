80097abc7b7747953c8544980fcfc17000d8fe38
Petar9512 <petarjelicic95@gmail.com>
2024-09-09 20:21:28 +0200

    update

--- CSHARP/Ucenje/UcenjeCS/KonzolnaAplikacija/ObradaSmjer.cs
+++ CSHARP/Ucenje/UcenjeCS/KonzolnaAplikacija/ObradaSmjer.cs
@@ -83,16 +83,52 @@ namespace UcenjeCS.KonzolnaAplikacija
         private void ObrisiSmjer()
         {
             PrikaziSveSmjerove();
-            Smjerovi.RemoveAt(Pomocno.UcitajRasponBroja("Odaberite redni broj smjera za brisanje", 1, int.MaxValue) - 1);
+            var s = Smjerovi[Pomocno.UcitajRasponBroja("Unesite redni broj smjera za brisanje", 1, Smjerovi.Count) - 1];
+            
+            if (Pomocno.UcitajBool("Sigurno obrisati smjer " + s.Naziv + "? (DA / NE)", "da"))
+            {
+                Smjerovi.Remove(s);
+            }
         }
 
         private void PromijeniSmjer()
         {
             PrikaziSveSmjerove();
-            var s = Smjerovi[Pomocno.UcitajRasponBroja("Odaberite redni broj smjera", 1, Smjerovi.Count) - 1];
+            var s = Smjerovi[Pomocno.UcitajRasponBroja("Odaberite redni broj smjera za promjenu", 1, Smjerovi.Count) - 1];
 
-            s.Sifra = Pomocno.UcitajRasponBroja("Unesite novu vrijednost šifre (" + s.Sifra + ")", 1, int.MaxValue);
-            s.Naziv = Pomocno.UcitajString("Unesite novu vrijednost naziva (" + s.Naziv + ")", 50, true);
+            if (Pomocno.UcitajRasponBroja("1. Promijeni sve \n2. Pojedinačna promjena", 1, 2) == 1)
+            {
+                s.Sifra = Pomocno.UcitajRasponBroja("Unesite novu vrijednost šifre (" + s.Sifra + ")", 1, int.MaxValue);
+                s.Naziv = Pomocno.UcitajString("Unesite novu vrijednost naziva (" + s.Naziv + ")", 50, true);
+                s.Trajanje = Pomocno.UcitajString("Unesite novo trajanje smjera (" + s.Trajanje + ")", 50, true);
+                s.IzvodiSeOd = DateOnly.FromDateTime(Pomocno.UcitajDatum(true));
+                s.Cijena = Pomocno.UcitajDecimalniBroj("Unesite novu cijenu (" + s.Cijena + ")", 1, 5000);
+                s.Vaucer = Pomocno.UcitajBool("Je li smjer vaučer? (DA / NE)", "da");
+            }
+            else
+            {
+                switch(Pomocno.UcitajRasponBroja("1. Šifra \n2. Naziv \n3. Trajanje \n4. Početak \n5. Cijena \n6. Vaučer", 1, 6))
+                {
+                    case 1:
+                        s.Sifra = Pomocno.UcitajRasponBroja("Unesite novu šifru", 1, int.MaxValue);
+                        break;
+                    case 2:
+                        s.Naziv = Pomocno.UcitajString(s.Naziv, "Unesite novi naziv", 50, true);
+                        break;
+                    case 3:
+                        s.Trajanje = Pomocno.UcitajString(s.Trajanje, "Unesite novo trajanje", 50, true);
+                        break;
+                    case 4:
+                        s.IzvodiSeOd = DateOnly.FromDateTime(Pomocno.UcitajDatum(true));
+                        break;
+                    case 5:
+                        s.Cijena = Pomocno.UcitajDecimalniBroj("Unesite novi cijenu", 1, 5000);
+                        break;
+                    case 6:
+                        s.Vaucer = Pomocno.UcitajBool("Je li smjer vaučer? DA / NE", "da");
+                        break;
+                }
+            }
         }
 
         private void DodajNoviSmjer()
@@ -100,7 +136,11 @@ namespace UcenjeCS.KonzolnaAplikacija
             Smjerovi.Add(new()
             {
                 Sifra = Pomocno.UcitajRasponBroja("Unesite šifru smjera", 1, int.MaxValue),
-                Naziv = Pomocno.UcitajString("Unesite naziv smjera", 50, true)
+                Naziv = Pomocno.UcitajString("Unesite naziv smjera", 50, true),
+                Trajanje = Pomocno.UcitajString("Unesite trajanje smjera", 50, true),
+                IzvodiSeOd = DateOnly.FromDateTime(Pomocno.UcitajDatum(true)),
+                Cijena = Pomocno.UcitajDecimalniBroj("Unesite cijenu smjera", 1, 5000),
+                Vaucer = Pomocno.UcitajBool("Je li smjer vaučer? (DA / NE)", "da")
             });
         }
 

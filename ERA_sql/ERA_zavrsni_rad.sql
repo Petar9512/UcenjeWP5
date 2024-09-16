8a58158a0ba8841a3f5ac7eea967e2d8d4a49ba5
Petar9512 <petarjelicic95@gmail.com>
2024-09-16 19:15:00 +0200

    promjena u bazi fakultet

--- ERA_sql/ERA_zavrsni_rad.sql
+++ ERA_sql/ERA_zavrsni_rad.sql
@@ -7,13 +7,13 @@ go
 use fakultet;
 
 create table Smjerovi(
-smjerID int not null primary key identity(1,1),
+sifra int not null primary key identity(1,1),
 naziv varchar(100) not null,
 brojStudenata int
 );
 
 create table Kolegiji(
-kolegijID int not null primary key identity(1,1),
+sifra int not null primary key identity(1,1),
 smjerID int not null,
 naziv varchar(100) not null,
 predavac varchar(50),
@@ -21,7 +21,7 @@ obavezni bit
 );
 
 create table Studenti(
-studentID int not null primary key identity(1,1),
+sifra int not null primary key identity(1,1),
 smjerID int not null,
 ime varchar(30) not null,
 prezime varchar(30) not null,
@@ -29,24 +29,25 @@ oib char(11)
 );
 
 create table IspitniRok(
-ispitniRokID int not null primary key identity(1,1),
+sifra int not null primary key identity(1,1),
 kolegijID int not null,
 vrstaIspita varchar(50) not null,
 datum datetime not null
 );
 
 create table Prijava(
+sifra int not null primary key identity(1,1),
 studentID int not null,
 ispitniRokID int not null,
 pristupio bit
 );
 
 
-alter table Kolegiji add foreign key (smjerID) references Smjerovi(smjerID);
-alter table Studenti add foreign key (smjerID) references Smjerovi(smjerID);
-alter table IspitniRok add foreign key (kolegijID) references Kolegiji(kolegijID);
-alter table Prijava add foreign key(studentID) references Studenti(studentID);
-alter table Prijava add foreign key(ispitniRokID) references IspitniRok(ispitniRokID);
+alter table Kolegiji add foreign key (smjerID) references Smjerovi(sifra);
+alter table Studenti add foreign key (smjerID) references Smjerovi(sifra);
+alter table IspitniRok add foreign key (kolegijID) references Kolegiji(sifra);
+alter table Prijava add foreign key(studentID) references Studenti(sifra);
+alter table Prijava add foreign key(ispitniRokID) references IspitniRok(sifra);
 
 
 insert into Smjerovi (naziv,brojStudenata) values

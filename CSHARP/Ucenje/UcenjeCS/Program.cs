d4a9fc33103bc6a0dad7a1924b92564f222defd7
Petar9512 <petarjelicic95@gmail.com>
2024-09-23 23:10:43 +0200

    .

--- CSHARP/Ucenje/UcenjeCS/Program.cs
+++ CSHARP/Ucenje/UcenjeCS/Program.cs
@@ -6,7 +6,7 @@ using UcenjeCS.CiklicneTablice;
 using UcenjeCS.GeneratorLozinki;
 using UcenjeCS.LjubavniKalkulator;
 
-new UcenjeCS.ConsoleAppFakultet.Izbornik();
+new UcenjeCS.KonzolnaAplikacija.Izbornik();
 //new UcenjeCS.E15KlasaObjekt.GenericiLambdaEkstenzije.Program();
 //new UcenjeCS.RandomKlaseObjekti.Random6.Program();
 //E01UlazIzlaz.Izvedi();

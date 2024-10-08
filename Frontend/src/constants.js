9ca5a7fae0f5a6f6fc3f113582c5da09a9438ea9
Petar9512 <petarjelicic95@gmail.com>
2024-10-08 21:32:47 +0200

    .

--- Frontend/src/constants.js
+++
@@ -1,9 +0,0 @@
-
-export const RouteNames = {
-HOME: '/',
-SMJER_PREGLED: '/smjerovi',
-SMJER_NOVI: '/smjerovi/dodaj',
-SMJER_PROMJENA: '/smjerovi/:sifra'
-}
-
-export const BACKEND_URL = 'https://sandworm-001-site1.ftempurl.com/api/v1'
